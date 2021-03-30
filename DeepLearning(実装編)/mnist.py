0111fde1b49157e46cf24dcd48a28f4c712efd77
伊藤 優太 <y-ito@plusa.co.jp>
2021-03-30 14:09:26 +0900

    手書き文字認識

--- DeepLearning(実装編)/mnist.py
+++ DeepLearning(実装編)/mnist.py
@@ -26,5 +26,21 @@ img_rows, img_cols = 28, 28
 
 # 学習データとテストデータに分割したデータ
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-print(x_train.shape)
+if k.image_data_format() == 'channels_first':
+    # 1次元配列に変換
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (img_rows, img_cols, 1)
+else:
+    # 1次元配列に変換
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+# 入力データの各画素を0-1の範囲で正規化
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train_shape:', x_train.shape)
 # %%
